e4614c127d7db5091ac5f2005ad7e22a8d976455
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-06-12 11:23:55 +0300

    close btn
    виправила ситуацію з кнопкою

---
+++ src/js/partials/modal.js
@@ -0,0 +1,20 @@
+import { testRefs } from "../templates/modal-tmpl";
+import { modalCardMarkUp } from "../templates/modal-tmpl";
+
+export function renderModalCard() {     
+    testRefs.backdrop.classList.remove('is-hidden');
+    testRefs.modalContainer.insertAdjacentHTML('beforeend', modalCardMarkUp({about: `Four of the West’s most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they’ve been double crossed – but by who and how?`,
+        imgSrc: "./images/modal-test/rectangle-18-mobile.jpg",
+        imgAlt: "test",
+        originalTitle: "A FISTFUL OF LEAD",
+        vote: "7.3",
+        votes: "1260",
+        popularity: "100.2",
+        genre: "Western",
+    }));
+    
+    testRefs.sourseInModalCardMobile.setAttribute("type", "image/jpeg");
+    testRefs.sourseInModalCardTablet.setAttribute("type", "image/jpeg");
+    testRefs.sourseInModalCardDesktop.setAttribute("type", "image/jpeg");
+    testRefs.imgInModalCardPicture.setAttribute("type", "image/jpeg");
+}
--- src/js/templates/modal-tmpl.js
+++ src/js/templates/modal-tmpl.js
@@ -1,37 +1,40 @@
 // у index.js підключаємо
-// import { testRefs } from "./js/templates/modal-tmpl";
-// import { renderModalCard } from "./js/templates/modal-tmpl";
-// і навішуємо слухач поки на імпровізовану кнопку, потім на картку фільму
-// testRefs.testOpenModalCard.addEventListener('click', renderModalCard);
+//  import { renderModalCard } from "./js/partials/modal";
+// замість того,що був
+// був такий: import { renderModalCard } from "./js/templates/modal-tmpl";
+
 
 export const testRefs = {
     backdrop: document.querySelector('#backdrop-modal-card'),
     testOpenModalCard: document.querySelector('#try-to-open-modal-card'),
-    // цей refs тимчасовий, бо у нас ще не всі елементи є на сторінці 
+    modalContainer: document.querySelector('#modal'),
+    sourseInModalCardDesktop: document.querySelector('#sourse-in-modal-card-desktop'),
+    sourseInModalCardTablet: document.querySelector('#sourse-in-modal-card-tablet'),
+    sourseInModalCardMobile: document.querySelector('#sourse-in-modal-card-mobile'),
+    imgInModalCardPicture: document.querySelector('#img-in-modal-card-picture'),
+    // цей refs тимчасовий, бо у нас ще не всі елементи є на сторінці
 }
 
 
 
-function modalCardMarkUp({ about, imgSrc, imgAlt, originalTitle, vote, votes, popularity, genre }) { 
+export function modalCardMarkUp({ about, imgSrc, imgAlt, originalTitle, vote, votes, popularity, genre }) { 
     
-    //це для прикладу
-    const srcsetMobile = "./images/modal-test/growing-mobile.jpg 1x, ./images/modal-test/growing-mobile@2x.jpg 2x";
-    const srcsetTablet = "./images/modal-test/growing-tablet.jpg 1x, ./images/modal-test/growing-tablet@2x.jpg 2x";
-    const srcsetDesktop = "./images/modal-test/growing-desctop.jpg 1x, ./images/modal-test/growing-desctop@2x.jpg 2x";
-
+    //посилання для прикладу
+    const srcsetMobile = "./images/modal-test/rectangle-18-mobile.jpg 240w, ./images/modal-test/rectangle-18-mobile@2x.jpg 480w";
+    const srcsetTablet = "./images/modal-test/rectangle-18-tablet.jpg 264w, ./images/modal-test/rectangle-18-tablet@2x.jpg 528w";
+    const srcsetDesktop = "./images/modal-test/rectangle-18-desktop.jpg 375w, ./images/modal-test/rectangle-18-desktop@2x.jpg 750w";
+    
     return  `
-    <div class="modal-card container" id="modal">
-        <button type="button" class="modal-card__close-btn" id="modal-close" title="close window" data-modal-close width="30" height="30">
-            <svg class="modal-card__close-btn-icon" width="14" height="14">
-                <use href="./images/icons.svg#icon-close"></use>
-            </svg>
-        </button>
+           
+        <div class="modal-card-thumb__img" id="modal">
         <picture class="modal-card__img">
-            <source media='(min-width:1280px)' srcset="${srcsetDesktop}">
-            <source media='(min-width:768px)' srcset="${srcsetTablet}">
-            <source media='(min-width:320px)' srcset="${srcsetMobile}">
-            <img src="${imgSrc}" alt="${imgAlt}" loading="lazy"/>
-        </picture>       
+            <source media='(min-width:1280px)' srcset="${srcsetDesktop}" sizes="375px" type="image/jpeg" id="sourse-in-modal-card-desktop">
+            <source media='(min-width:768px)' srcset="${srcsetTablet}" sizes="264px" type="image/jpeg" id="sourse-in-modal-card-tablet">
+            <source media='(min-width:320px)' srcset="${srcsetMobile}" sizes="240px" type="image/jpeg" id="sourse-in-modal-card-mobile">
+            <img src="${imgSrc}" alt="${imgAlt}" loading="lazy" type="image/jpeg id="img-in-modal-card-picture"/>
+        </picture>
+        </div>   
+            
         <div class="modal-card-thumb__content">
             <div class="modal-card-info">
                 <h2 class="modal-card-info__title">${originalTitle}</h2>
@@ -61,11 +64,6 @@ function modalCardMarkUp({ about, imgSrc, imgAlt, originalTitle, vote, votes, po
                 <button type="button" id="queue-add" title="add to Queue" class="modal-card-btn__queue">add to queue</button>
             </div>
         </div>
-    </div>
+    
     `
-}
-
-export function renderModalCard() {     
-    testRefs.backdrop.classList.remove('is-hidden');
-    testRefs.backdrop.insertAdjacentHTML('beforeend', modalCardMarkUp({}));
 }
