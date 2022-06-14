f56a36af00cc540bd7ef92ee74f98dfa2268edcc
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-06-14 16:09:53 +0300

    виправила скролл
    заборонила скролл сторінки при відкритій модалці
    змінила падінг контейнера, щоб більше інформації влазило
    забрала кривий маржин, що був в макеті
    прибрала горизонтальний скролл

--- src/js/templates/modal.js
+++ src/js/templates/modal.js
@@ -1,9 +1,12 @@
 import { testRefs } from "../templates/modal-tmpl";
 import { modalCardMarkUp } from "../templates/modal-tmpl";
-
+import { REFS } from "../site-constants";
 
 export function renderModalCard() {     
     testRefs.backdrop.classList.remove('js-hidden');
+    REFS.body.classList.add('js-modal-is-open');
+    // REFS.body.classList.remove('js-modal-is-open'); 
+    //це треба вставити при обробці кліку по кнопці закриття модалки чи backdrop`y чи клавіші esc
 
     const string = modalCardMarkUp({
         about: `Four of the West’s most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they’ve been double crossed – but by who and how?`,
