a5a17be84acaba25915b5e14b1193c88319c743a
Julanna11 <joy93julanna@gmail.com>
2022-06-15 19:25:46 +0300

    close modal btn
    import changed, evlistener deleted

--- src/js/templates/modal-close-btn.js
+++ src/js/templates/modal-close-btn.js
@@ -1,28 +1,28 @@
 import { REFS } from "../site-constants";
 
-const { body, backdrop, modalCloseBtn, modalContainer} = REFS;
+const { body, backdrop, modalCloseBtn } = REFS;
 
 modalCloseBtn.addEventListener('click', closeModal);
 backdrop.addEventListener('click', onClickBackdrop);
 window.addEventListener('keydown', onCloseEscKey);
 
-export function closeModal() {
+function closeModal() {
 backdrop.classList.add('js-hidden');
-modalContainer.innerHTML = '';
   //for body no-scroll   //
-body.classList.remove('js-modal-is-open');
+    body.classList.remove('js-modal-is-open');
 }
 
-export function onClickBackdrop(e) {
+function onClickBackdrop(e) {
     if (e.target === e.currentTarget) {
         closeModal();
-        modalContainer.innerHTML = '';
     }
   }
 
-export function onCloseEscKey(e) {
+function onCloseEscKey(e) {
   if (e.key === 'Escape') {
     closeModal();
     window.removeEventListener('keydown', onCloseEscKey);
   }
 }
+
+export { closeModal, onClickBackdrop, onCloseEscKey };
