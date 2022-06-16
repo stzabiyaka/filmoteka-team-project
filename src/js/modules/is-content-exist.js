f921a4a6ef100cda39c822e26b609127bc063414
kolya8077 <kolya8077@gmail.com>
2022-06-16 20:58:19 +0300

    Complit

--- src/js/modules/is-content-exist.js
+++ src/js/modules/is-content-exist.js
@@ -1,13 +1,29 @@
+// Імпорт в index
 // import isContentExist from './js/modules/is-content-exist';
+
+// додаємо коментар
+// const notification = 'От халепа(( зєднання з сервером відсутнє';
+
+// Виклик функції для тесту
+// isContentExist({
+//   isContent: false,
+//   notification: notification,
+// });
 
 
 import { REFS } from '../site-constants';
 
-export default function isContentExist({ isContent, callback }) {
+export default function isContentExist({ isContent, callback, notification }) {
   if (isContent) {
     REFS.libraryContainer.addEventListener('click', callback);
     return;
   }
+
+  REFS.libraryContainer.innerHTML = `
+  <div class="library__notification-conteiner">
+    <p class="library__notification">${notification}</p>
+  </div>
+  `;
 
   REFS.libraryContainer.removeEventListener('click', callback);
 }
