415430c43af3e3b0ec6ddfefa7e3cc22c2b8d6f0
Marina <sheremetamaryna@gmail.com>
2022-06-17 14:13:15 +0200

    search-film  handleSearch

--- src/js/modules/search-film.js
+++ src/js/modules/search-film.js
@@ -6,19 +6,17 @@ function validateLength (event) {
     let form = event.currentTarget.value.trim();
     console.log(form);
     const str = this.value;
-
-    if(this.value.length < 2) {
-        console.log('Search was not successful. Please, enter another movie name and try again');
-       return false
-   } else if ((/^([а-яА-ЯёЁ]*)$/.test(str))) {
+    if ((/^([\s%&#@])*$/.test(str)) || (/^([>(.*?)<])*$/.test(str))) {
+        (console.log('недопустимі символи & теги')) 
+        return;
+    }
+    if ((/^([а-яА-ЯёЁ]*)$/.test(str))) {
        (console.log('тільки кирилиця'))
-   } else if  ((/^([a-zA-Z]*)$/.test(str))) {
-       (console.log('тільки латиниця'))
-   } else if ((/^([\s%&#@])*$/.test(str))) {
-       (console.log('недопустимі символи'))
-   } else if ((/^([>(.*?)<])*$/.test(str))) {
-       (console.log('теги HTML'))
-   }
+   } 
+   if(this.value.length < 2) {
+    console.log('Search was not successful. Please, enter another movie name and try again');
+   return false;
+   } 
        console.log (`HOORAY! Found total_results movies`)
        return str
        
