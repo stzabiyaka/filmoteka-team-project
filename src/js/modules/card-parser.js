0d5eb9a8bcb9700e9cac4c025ee23dc8f6bbe60f
Julanna11 <joy93julanna@gmail.com>
2022-06-20 20:28:37 +0300

    poster-inprogress

--- src/js/modules/card-parser.js
+++ src/js/modules/card-parser.js
@@ -1,23 +1,35 @@
-import ImagesPathConstructor from "../classes/utilities/images-path-constructor";
+import ImagesPathConstructor from '../classes/utilities/images-path-constructor';
+import noPoster from '../../images/poster.svg';
 
-export default function cardParser ({ genres, poster_path, release_date, title, vote_average, id, captions }) {    
-    if (genres.length === 0) {
-        return
-    }
-    let image = '/poster.jpg';
-    let imageRetina = '/poster.jpg';
-    if (poster_path){
-        image = ImagesPathConstructor.getImagePath({ path:poster_path });
-        imageRetina = ImagesPathConstructor.getImagePath({ path: poster_path, width: 'retina' });
-    }
-    release_date = release_date.slice(0, 4);
-    genres = genres.map (genre => genre);
-    if ( genres.length > 2) {
-        genres = genres.slice(0,2);
-        genres.push(captions.movieDetails.other);
-    }
-   const poster = `${title} ${captions.movieDetails.poster}`;
-    return `<a  class="movie-card" data-movie-id="${id}"><article>
+export default function cardParser({
+  genres,
+  poster_path,
+  release_date,
+  title,
+  vote_average,
+  id,
+  captions,
+}) {
+  if (genres.length === 0) {
+    return;
+  }
+  let image = noPoster;
+  let imageRetina = noPoster;
+  if (poster_path) {
+    image = ImagesPathConstructor.getImagePath({ path: poster_path });
+    imageRetina = ImagesPathConstructor.getImagePath({
+      path: poster_path,
+      width: 'retina',
+    });
+  }
+  release_date = release_date.slice(0, 4);
+  genres = genres.map(genre => genre);
+  if (genres.length > 2) {
+    genres = genres.slice(0, 2);
+    genres.push(captions.movieDetails.other);
+  }
+  const poster = `${title} ${captions.movieDetails.poster}`;
+  return `<a  class="movie-card" data-movie-id="${id}"><article>
     <h2 class="visually_hidden">${title}</h2>
     <picture>
         <source srcset="${image}, 1x, ${imageRetina}, 2x" type="image/jpeg">
